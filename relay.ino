1cd4e1d84ccd653128ba3ee749ca26ecf954bccf
Tianyu Liu <info@jamesvillage.dev>
2019-11-25 23:16:40 +0100

    Add temp extractor, WIP, temp working
    Signed-off-by: Tianyu Liu <info@jamesvillage.dev>

--- relay.ino
+++ relay.ino
@@ -1,16 +1,53 @@
 #include "src/support/wifi/wireless.h"
 #include <PubSubClient.h>
+#include <math.h>
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-static void callback(const char* topic, byte* payload, unsigned int length)
+#define FLOAT_DIGITS (5)
+
+static float byte_to_float(byte* bytes)
+{
+    float retval = 0;
+    char value[FLOAT_DIGITS];
+    memcpy(value, bytes, 5);
+    int16_t dot_place = 0;
+    for (uint32_t i = 0; i < FLOAT_DIGITS; ++i)
+    {
+        if (value[i] == 46)
+        {
+            dot_place = i;
+        }
+    }
+
+    for (int32_t i = 0; i < FLOAT_DIGITS; ++i)
+    {
+        float dig = 0;
+        if (i != dot_place)
+        {
+            int exp = (i < dot_place)? (dot_place - i - 1) : (dot_place - i);
+            dig = (value[i] - 48) * (powf(10, exp));
+            retval += dig;
+        }
+    }
+
+    return retval;
+}
+
+static void callback(char* topic, byte* payload, unsigned int length)
 {
     Serial.print(topic);
     Serial.print("\n");
     Serial.print((char*)payload);
     Serial.print("\n");
-    if (memcmp(payload, "on", 3) == 0)
+    if (memcmp(topic, "home/temperature", 16) == 0)
+    {
+        Serial.print("convert\n");
+        float temp = byte_to_float(payload);
+        Serial.print(temp);
+    }
+    if (memcmp(payload, "heat", 4) == 0)
     {
         digitalWrite(16, LOW);
     }
@@ -18,7 +55,7 @@ static void callback(const char* topic, byte* payload, unsigned int length)
     {
         digitalWrite(16, HIGH);
     }
-    memset(payload, 0, 4);
+    memset(payload, 0, 16);
 }
 void setup()
 {
@@ -43,6 +80,11 @@ void setup()
     }
     Serial.print("Subscribing..");
     result = client.subscribe("boiler/command");
+    client.subscribe("boiler/temperature_sp");
+    client.subscribe("boiler/temperature_low_sp");
+    client.subscribe("boiler/temperature_high_sp");
+    result = client.subscribe("home/temperature");
+    client.publish("boiler/available", "online");
 
     if (result)
     {
